e42b83757cf51315c9bba76b6c34c7b203ff96fe
Andrew Sherlock <sherlock-r32@live.ie>
2018-04-03 21:40:55 +0100

    Beginning work on testing

---
+++ tests/ControllerTests/DefaultControllerTest.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: andrew
+ * Date: 03/04/2018
+ * Time: 20:17
+ */
+
+namespace App\Tests\ControllerTests;
+
+
+class DefaultControllerTest
+{
+
+}
---
+++ tests/DefaultControllerTest.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: andrew
+ * Date: 03/04/2018
+ * Time: 19:52
+ */
